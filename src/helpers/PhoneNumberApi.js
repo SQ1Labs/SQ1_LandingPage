e75456966c46b324e96ac4382e66be52e6832ada
dangerstephen <dangersteve1@gmail.com>
2017-05-01 23:21:50 -0700

    working on deploying

--- src/helpers/PhoneNumberApi.js
+++ src/helpers/PhoneNumberApi.js
@@ -5,12 +5,12 @@ import _superagent from 'superagent';
 
 const superagent = superagentPromise(_superagent, global.Promise);
 
-const API_ROOT = 'https://squareonelabs-io.herokuapp.com/starthere';
+const API_ROOT = 'https://squareonelabs.herokuapp.com/starthere';
 
 const responseBody = res => res.body;
 
 const PhoneNumber = {
-	post: numberRefsObject => superagent.post(API_ROOT).set('X-Parse-Application-Id':'squareone').send(numberRefsObject).then()
+	post: numberRefsObject => superagent.post(API_ROOT).set('X-Parse-Application-Id':'engage').send(numberRefsObject).then()
 }
 
 export default {
